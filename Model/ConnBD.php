7baf151d253dabac11884c6fd6e319a95a9425b5
Sebastian Rodriguez <seba290306@gmail.com>
2023-06-12 18:16:16 -0600

    V2
    Conexion

--- Model/ConnBD.php
+++ Model/ConnBD.php
@@ -1,3 +1,36 @@
 <?php
+class Conexion{
+    public function Conectar(){
+    define('HOST', '127.0.0.1');
+    define('PORT', 1522);
+    define('NAME', 'orcl');
+    define('USER', '');
+    define('PASS', '');
 
-// git
+    $bd_settings = " (DESCRIPTION =
+     (ADDRESS = 
+     (PROTOCOL = TCP)
+     (HOST = " . HOST . ")
+     (PORT = " . PORT . ")
+     )
+     (CONNECT_DATA =
+     (SERVER = DEDICATED)
+    (SERVICE_NAME = " . NAME . ")
+  )
+)
+";
+try{
+  $bd = new PDO('oci:dbname='.$bd_settings, USER, PASS);
+  $bd->setAttribute(PDO::ATTR_CASE, PDO::CASE_LOWER);
+  $bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo'CONEXION EXITOSA';
+  return $bd;
+
+} catch(Exception $e){
+echo "ERROR DE CONEXION: ".$e->getMessage();
+}
+
+  }
+}
+
+?>
